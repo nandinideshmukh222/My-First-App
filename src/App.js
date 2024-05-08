0e14b79b8a2d877f4e93550fe4646c3334d15267
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-05-08 21:10:17 +0530

    my first app react project
    my first app react project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>SocialMedia</h2>
+        <ul>
+          <li>
+            Facebook - <a href="https://www.facebook.com/">Facebook Profile</a>{" "}
+          </li>
+          <li>
+            LinkedIn -{" "}
+            <a href="https://www.google.com/search?q=linked">
+              Profetional Profile
+            </a>{" "}
+          </li>
+          <li>
+            Instagram -{" "}
+            <a href="https://www.instagram.com/ target=_blank">Insta_Profile</a>{" "}
+          </li>
+          <li>
+            Youtube -{" "}
+            <a href="https://www.youtube.com/ target=_blank">
+              Please Like and Subscribe
+            </a>{" "}
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+export default App;
