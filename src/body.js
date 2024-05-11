4b916d266894639110d88e8c280657fa17d53a78
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-05-11 21:41:15 +0530

    nav bar setting
    nav bar setting

---
+++ src/body.js
@@ -0,0 +1,16 @@
+function Body(){
+    
+    return(
+   
+
+<p class="para">
+
+<h2><strong>My Personal details</strong></h2>
+<strong>Name</strong>: Komal Nimbalkar
+<h2><strong>Contanct Details : </strong></h2>
+<p><strong>Mobile</strong>: 1234567890 </p>
+<strong>Email</strong>: nandinideshmukh222@gmail.com
+</p>
+);
+}
+export default Body;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
-
+import Head from './head';
+import Body from './body';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/*<App />*/}
+   <Head/>
+   <Body/>
   </React.StrictMode>
 );
 
